08b37c033653111cd70549d7c34ec9aeaaeec3be
jsalmero <johana-salmeron1@hotmail.com>
2018-11-07 07:47:35 -0500

    actualizacion

--- MyApplication4/app/src/main/java/com/example/redes/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/redes/myapplication/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.redes.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import java.lang.Override;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,47 +12,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("EStoy en Create");
     }
-
+    //@Override
     protected void onStart() {
         super.onStart();
         setContentView(R.layout.activity_main);
         System.out.println("EStoy en Start");
     }
-
+           //@Override
     protected void onResume() {
         super.onResume();
         setContentView(R.layout.activity_main);
         System.out.println("EStoy en Resume");
     }
-
+ // @Override
     protected void onPause() {
         super.onPause();
         setContentView(R.layout.activity_main);
         System.out.println("EStoy en Pause");
     }
-
+   // @Override
     protected void onStop() {
         super.onStop();
         setContentView(R.layout.activity_main);
         System.out.println("EStoy en Stop");
     }
-
+        // @Override
     protected void onRestart() {
         super.onRestart();
         setContentView(R.layout.activity_main);
         System.out.println("EStoy en Restart");
     }
+
     protected void onDestroy() {
         super.onDestroy();
         setContentView(R.layout.activity_main);
         System.out.println("EStoy en Destroy");
     }
-    /*@Override
-    protected void onStart(Bundle savedInstanceState) {
-        super.onStart();
-        setContentView(R.layout.activity_main);
-        System.out.println("EStoy en Start");*/
-    }
-
-
-
+}
